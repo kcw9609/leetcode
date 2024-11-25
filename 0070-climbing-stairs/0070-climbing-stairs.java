2c994f7afcef70a7fb429e78e3794f2f0b038ab9
Chaewon Kang <101781313+kcw9609@users.noreply.github.com>
2024-11-26 08:57:11 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (42.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,3 +1,4 @@
+/** 
 class Solution {
     public int climbStairs(int n) {
         // 초기화
@@ -16,8 +17,27 @@ class Solution {
         }
         return dp[n];
 
+}
+*/
+class Solution {
+    public int climbStairs(int n) {
+        // 초기화
+        int prev1=1, prev2 = 1;
+        int current = 0;
 
-    
-        
+
+// 범위 처리
+        if(n == 0 || n== 1 ) return 1;
+
+        // 다이나믹
+        for(int i = 2 ; i <= n ; i ++) {
+            //  ex n=3, 2층까지 올라가는 수 + 1층 올라가는 수
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+            
+        }
+        return prev1;
+     
     }
 }
